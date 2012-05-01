b8dbf4cb62e91a48da483fc5c2d3db456c34b201
rzander_cp <SND\rzander_cp>
2012-05-01 09:15:06 +0000

    Initial upload

---
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/AgentSettingItem.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using sccmclictr.automation;
+
+namespace ClientCenter
+{
+    /// <summary>
+    /// Interaction logic for AgentSettingItem.xaml
+    /// </summary>
+    public partial class AgentSettingItem : UserControl
+    {
+        private SCCMAgent oAgent;
+        public AgentSettingItem()
+        {
+            InitializeComponent();
+        }
+
+        public SCCMAgent SCCMAgentConnection
+        {
+            get
+            {
+                return oAgent;
+            }
+            set
+            {
+                if (value.isConnected)
+                {
+                    oAgent = value;
+                    AgentSettings.IsEnabled = true;
+
+                    Mouse.OverrideCursor = Cursors.Wait;
+
+                    imgSiteCode_MouseLeftButtonDown(this, null);
+                    imgAgentVersion_MouseLeftButtonDown(this, null);
+                    imgMP_MouseLeftButtonDown(this, null);
+
+                    Mouse.OverrideCursor = Cursors.Arrow;
+
+                }
+            }
+        }
+
+        private void imgSiteCode_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            tbSiteCode.Text = oAgent.Client.AgentProperties.AssignedSite;
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgAgentVersion_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            tbAgentVersion.Text = oAgent.Client.AgentProperties.ClientVersion;
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgMP_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            tbMP.Text = oAgent.Client.AgentProperties.ManagementPoint;
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgINetMP_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            tbInetMP.Text = oAgent.Client.AgentProperties.ManagementPointInternet;
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+    }
+}
---
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Stema.Controls;
+using sccmclictr.automation;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace ClientCenter
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public SCCMAgent oAgent;
+        public MyTraceListener myTrace;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ThemeManager.SetActiveTheme(NavigationPaneTheme.WindowsLive);
+            Style s = new Style();
+            s.Setters.Add(new Setter(UIElement.VisibilityProperty, Visibility.Collapsed));
+            tabNavigationPanels.ItemContainerStyle = s;
+        }
+
+        private void bt_Connect2_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+
+            ConnectionDock.Visibility = System.Windows.Visibility.Collapsed;
+
+            oAgent = new SCCMAgent(tb_TargetComputer2.Text, null, null);
+            oAgent.connect();
+            MyTraceListener myTrace = new MyTraceListener(ref tStatus);
+            myTrace.TraceOutputOptions = TraceOptions.None;
+            oAgent.PSCode.Listeners.Add(myTrace);
+
+            agentSettingItem1.SCCMAgentConnection = oAgent;
+            
+            navigationPane1.IsEnabled = true;
+
+            Mouse.OverrideCursor = Cursors.Arrow;
+           
+        }
+
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            if (((System.Windows.Controls.HeaderedItemsControl)(e.Source)).Tag != null)
+            {
+                
+                foreach (TabItem iTab in tabNavigationPanels.Items)
+                {
+                    if (iTab.Tag.ToString() == ((System.Windows.Controls.HeaderedItemsControl)(e.Source)).Tag.ToString())
+                    {
+                        iTab.IsSelected = true;
+
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public class MyTraceListener : TraceListener, INotifyPropertyChanged
+    {
+        TextBox oOutTB;
+        private readonly StringBuilder builder;
+
+        public MyTraceListener(ref TextBox TB)
+        {
+            oOutTB = TB;
+            this.builder = new StringBuilder();
+        }
+
+        public string Trace
+        {
+            get { return this.builder.ToString(); }
+        }
+
+        public override void Write(string message)
+        {
+            this.builder.Append(message.Replace("PSCode Information: 0 :", ""));
+            oOutTB.Text = oOutTB.Text + message.Replace("PSCode Information: 0 :", "");
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Trace"));
+        }
+
+        public override void WriteLine(string message)
+        {
+            this.builder.AppendLine(message.Replace("PSCode Information: 0 :", ""));
+            oOutTB.Text = oOutTB.Text + message.Replace("PSCode Information: 0 :", "") + "\r\n";
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Trace"));
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    } 
+
+
+
+}
