d8e8d82b6f383b6e68a3b2c6625da1c4cfc3c87c
rzander_cp <SND\rzander_cp>
2012-05-21 20:21:51 +0000

    Trace Output improved (Errors in Red); HTTP Ports, ProxyMP and SLP implemented

--- SCCMCliCtrWPF/SCCMCliCtrWPF/AgentSettingItem.xaml.cs
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/AgentSettingItem.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using System.Diagnostics;
 using sccmclictr.automation;
 
 namespace ClientCenter
@@ -21,6 +23,9 @@ namespace ClientCenter
     public partial class AgentSettingItem : UserControl
     {
         private SCCMAgent oAgent;
+
+        public MyTraceListener Listener;
+
         public AgentSettingItem()
         {
             InitializeComponent();
@@ -37,16 +42,17 @@ namespace ClientCenter
                 if (value.isConnected)
                 {
                     oAgent = value;
-                    AgentSettings.IsEnabled = true;
+                    spAgentSettings.IsEnabled = true;
+                    spHTTPPort.IsEnabled = true;
+                    spHTTPSPort.IsEnabled = true;
 
                     Mouse.OverrideCursor = Cursors.Wait;
 
                     imgSiteCode_MouseLeftButtonDown(this, null);
                     imgAgentVersion_MouseLeftButtonDown(this, null);
                     imgMP_MouseLeftButtonDown(this, null);
-
+                    
                     Mouse.OverrideCursor = Cursors.Arrow;
-
                 }
             }
         }
@@ -69,6 +75,13 @@ namespace ClientCenter
         {
             Mouse.OverrideCursor = Cursors.Wait;
             tbMP.Text = oAgent.Client.AgentProperties.ManagementPoint;
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgProxyMP_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            tbMP.Text = oAgent.Client.AgentProperties.ManagementPointProxy;
             Mouse.OverrideCursor = Cursors.Arrow;
         }
 
@@ -100,10 +113,107 @@ namespace ClientCenter
                 oAgent.Client.AgentProperties.AssignedSite = tbSiteCode.Text;
             else
             {
+                Listener.WriteError("Error: Site Code must have 3 characters.");
                 MessageBox.Show("Site Code must have 3 characters!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             Mouse.OverrideCursor = Cursors.Arrow;
         }
+
+        private void imgGetHTTPPort_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                tbHTTPPort.Text = oAgent.Client.AgentProperties.HTTPPort.ToString();
+            }
+            catch(Exception ex)
+            {
+                Listener.WriteError("Error: Unable to get the HTTP Port.");
+                Listener.WriteError(ex.Message);
+
+                tbHTTPPort.Text = "";
+            }
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgSetHTTPPort_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                if (int.Parse(tbHTTPPort.Text) > 0)
+                    oAgent.Client.AgentProperties.HTTPPort = int.Parse(tbHTTPPort.Text);
+            }
+            catch(Exception ex)
+            {
+                Listener.WriteError("Error: Unable to set the HTTP Port.");
+                Listener.WriteError(ex.Message);
+                MessageBox.Show("Unable to set the HTTP Port.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgGetHTTPSPort_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                tbHTTPSPort.Text = oAgent.Client.AgentProperties.HTTPSPort.ToString();
+            }
+            catch (Exception ex)
+            {
+                Listener.WriteError("Error: Unable to get the HTTPS Port.");
+                Listener.WriteError(ex.Message);
+
+                tbHTTPSPort.Text = "";
+            }
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgSetHTTPSPort_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                if (int.Parse(tbHTTPSPort.Text) > 0)
+                    oAgent.Client.AgentProperties.HTTPSPort = int.Parse(tbHTTPSPort.Text);
+            }
+            catch (Exception ex)
+            {
+                Listener.WriteError("Error: Unable to set the HTTPS Port.");
+                Listener.WriteError(ex.Message);
+                MessageBox.Show("Unable to set the HTTPS Port.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgGetSLP_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                tbSLP.Text = oAgent.Client.AgentProperties.ServerLocatorPoint;
+            }
+            catch (Exception ex)
+            {
+                Listener.WriteError(ex.Message);
+            }
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void imgSaveSLP_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                oAgent.Client.AgentProperties.ServerLocatorPoint = tbSLP.Text;
+            }
+            catch (Exception ex)
+            {
+                Listener.WriteError(ex.Message);
+            }
+        }
+
+
 
     }
 }
--- SCCMCliCtrWPF/SCCMCliCtrWPF/MainPage.xaml.cs
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/MainPage.xaml.cs
@@ -52,19 +52,27 @@ namespace ClientCenter
             catch { }
         }
 
-        private void bt_Connect2_Click(object sender, RoutedEventArgs e)
+        private void bt_Connect_Click(object sender, RoutedEventArgs e)
         {
             Mouse.OverrideCursor = Cursors.Wait;
-
+            string sTarget = tb_TargetComputer.Text;
             try
             {
-                oAgent = new SCCMAgent(tb_TargetComputer2.Text, null, null);
-                oAgent.connect();
-                MyTraceListener myTrace = new MyTraceListener(ref tStatus);
+                
+                if(sender == bt_Connect2)
+                    sTarget = tb_TargetComputer2.Text;
+
+                rStatus.Document = new FlowDocument();
+                myTrace = new MyTraceListener(ref rStatus);
                 myTrace.TraceOutputOptions = TraceOptions.None;
+
+                oAgent = new SCCMAgent(sTarget, null, null);
+                oAgent.connect();
+
                 oAgent.PSCode.Listeners.Add(myTrace);
 
                 agentSettingItem1.SCCMAgentConnection = oAgent;
+                agentSettingItem1.Listener = myTrace;
 
                 navigationPane1.IsEnabled = true;
 
@@ -77,42 +85,13 @@ namespace ClientCenter
                 ribbon1.IsEnabled = false;
                 navigationPane1.IsEnabled = false;
                 agentSettingItem1.IsEnabled = false;
+                myTrace.WriteError("Unable to connect: " + sTarget);
+                myTrace.WriteError(ex.Message);
                 MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
             Mouse.OverrideCursor = Cursors.Arrow;
            
-        }
-
-        private void bt_Connect_Click(object sender, RoutedEventArgs e)
-        {
-            Mouse.OverrideCursor = Cursors.Wait;
-
-            try
-            {
-                oAgent = new SCCMAgent(tb_TargetComputer.Text, null, null);
-                oAgent.connect();
-                MyTraceListener myTrace = new MyTraceListener(ref tStatus);
-                myTrace.TraceOutputOptions = TraceOptions.None;
-                oAgent.PSCode.Listeners.Add(myTrace);
-
-                agentSettingItem1.SCCMAgentConnection = oAgent;
-
-                navigationPane1.IsEnabled = true;
-
-                ConnectionDock.Visibility = System.Windows.Visibility.Collapsed;
-                ribbon1.IsEnabled = true;
-                agentSettingItem1.IsEnabled = true;
-            }
-            catch (Exception ex)
-            {
-                ribbon1.IsEnabled = false;
-                navigationPane1.IsEnabled = false;
-                agentSettingItem1.IsEnabled = false;
-                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            Mouse.OverrideCursor = Cursors.Arrow;
         }
 
         private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
@@ -199,18 +178,40 @@ namespace ClientCenter
             Mouse.OverrideCursor = Cursors.Arrow;
         }
 
+        private void rStatus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rStatus.ScrollToEnd();
+        }
 
+        bool isResizing = false; 
+        Point startPt;
+        private void PSGrip_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.Capture(this.PSGrip); 
+            startPt = e.GetPosition(this.PSGrip); 
+            isResizing = true;
+        }
 
+        private void PSGrip_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isResizing)
+            {
+                Point newPt = e.GetPosition(this.PSGrip);
+                StatusDock.Height -= newPt.Y - startPt.Y;
+                isResizing = false;
+                this.PSGrip.ReleaseMouseCapture();
+            }
+        }
     }
 
     public class MyTraceListener : TraceListener, INotifyPropertyChanged
     {
-        TextBox oOutTB;
+        RichTextBox oROutTB;
         private readonly StringBuilder builder;
 
-        public MyTraceListener(ref TextBox TB)
+        public MyTraceListener(ref RichTextBox TB)
         {
-            oOutTB = TB;
+            oROutTB = TB;
             this.builder = new StringBuilder();
         }
 
@@ -221,15 +222,36 @@ namespace ClientCenter
 
         public override void Write(string message)
         {
+            //Paragraph p = oROutTB.Document.Blocks.FirstBlock as Paragraph;
+            //p.LineHeight = 10; 
+
             this.builder.Append(message.Replace("PSCode Information: 0 :", ""));
-            oOutTB.Text = oOutTB.Text + message.Replace("PSCode Information: 0 :", "");
+            oROutTB.AppendText(message.Replace("PSCode Information: 0 :", ""));
+            
             this.OnPropertyChanged(new PropertyChangedEventArgs("Trace"));
         }
 
         public override void WriteLine(string message)
         {
             this.builder.AppendLine(message.Replace("PSCode Information: 0 :", ""));
-            oOutTB.Text = oOutTB.Text + message.Replace("PSCode Information: 0 :", "") + "\r\n";
+
+            TextRange tr = new TextRange(oROutTB.Document.ContentEnd, oROutTB.Document.ContentEnd);
+            tr.Text = message.Replace("PSCode Information: 0 :", "") + "\r\n";
+            tr.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Black);
+            tr.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Trace"));
+        }
+
+        public void WriteError(string message)
+        {
+            this.builder.AppendLine(message.Replace("PSCode Information: 0 :", ""));
+
+            TextRange tr = new TextRange(oROutTB.Document.ContentEnd, oROutTB.Document.ContentEnd);
+            tr.Text = message.Replace("PSCode Information: 0 :", "") + "\r\n";
+            tr.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Red);
+            tr.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+
             this.OnPropertyChanged(new PropertyChangedEventArgs("Trace"));
         }
 
