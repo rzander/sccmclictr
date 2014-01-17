4990fbc51070a92cf4577506d23fc224e9c2c838
rzander_cp <SND\rzander_cp>
2014-01-17 08:14:52 +0000

    Missing files added; cleanup

---
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/SWStatusGrid.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+using sccmclictr.automation;
+using sccmclictr.automation.functions;
+using System.IO;
+using System.Globalization;
+using System.ComponentModel;
+
+namespace ClientCenter.Controls
+{
+    /// <summary>
+    /// Interaction logic for ApplicationGrid.xaml
+    /// </summary>
+    public partial class SWStatusGrid : UserControl
+    {
+        private SCCMAgent oAgent;
+        public MyTraceListener Listener;
+        internal List<sccmclictr.automation.functions.softwaredistribution.SoftwareStatus> iSoftware;
+
+        public SWStatusGrid()
+        {
+            InitializeComponent();
+        }
+
+        public SCCMAgent SCCMAgentConnection
+        {
+            get
+            {
+                return oAgent;
+            }
+            set
+            {
+                if (value.isConnected)
+                {
+                    Mouse.OverrideCursor = Cursors.Wait;
+                    try
+                    {
+                        //SCCMAgent oAgent = new SCCMAgent("localhost");
+                        //List<softwaredistribution.CCM_Application> lApps = oAgent.Client.SoftwareDistribution.Applications.OrderBy(t => t.FullName).ToList();
+                        //lApps.ToString();
+
+                        oAgent = value;
+                        iSoftware = oAgent.Client.SoftwareDistribution.SoftwareSummary.GroupBy(t => t.Name).Select(grp => grp.FirstOrDefault()).Where(p=>p.Name != "").OrderBy(o => o.Name).ToList();
+                        //TEST.Source = BitMapConvert.ToBitmapImage(iApplications[0].IconAsImage);
+
+                        dataGrid1.BeginInit();
+                        dataGrid1.ItemsSource = iSoftware;
+                        dataGrid1.EndInit();
+                    }
+                    catch(Exception ex)
+                    {
+                        ex.Message.ToString();
+                    }
+                    Mouse.OverrideCursor = Cursors.Arrow;
+                }
+            }
+        }
+
+
+
+        private void bt_Reload_Click(object sender, RoutedEventArgs e)
+        {
+            iSoftware = oAgent.Client.SoftwareDistribution.SoftwareSummary_(true).GroupBy(t => t.Name).Select(grp => grp.FirstOrDefault()).Where(p => p.Name != "").OrderBy(o => o.Name).ToList();
+
+            List<SortDescription> ssort = GetSortInfo(dataGrid1);
+
+            dataGrid1.BeginInit();
+            dataGrid1.ItemsSource = iSoftware;
+            dataGrid1.EndInit();
+
+            SetSortInfo(dataGrid1, ssort);
+        }
+
+        private void bt_ReloadActive_Click(object sender, RoutedEventArgs e)
+        {
+            iSoftware = oAgent.Client.SoftwareDistribution.SoftwareSummary_(true).GroupBy(t => t.Name).Select(grp => grp.FirstOrDefault()).Where(p => p.Name != "").Where(p=>p.Status != "Installed").OrderBy(o => o.Name).ToList();
+
+            List<SortDescription> ssort = GetSortInfo(dataGrid1);
+
+            dataGrid1.BeginInit();
+            dataGrid1.ItemsSource = iSoftware;
+            dataGrid1.EndInit();
+
+            SetSortInfo(dataGrid1, ssort);
+        }
+
+        List<DataGridColumn> GetColumnInfo(DataGrid dg)
+        {
+            List<DataGridColumn> columnInfos = new List<DataGridColumn>();
+            foreach (var column in dg.Columns)
+            {
+                columnInfos.Add(column);
+            }
+            return columnInfos;
+        }
+
+        List<SortDescription> GetSortInfo(DataGrid dg)
+        {
+            List<SortDescription> sortInfos = new List<SortDescription>();
+            foreach (var sortDescription in dg.Items.SortDescriptions)
+            {
+                sortInfos.Add(sortDescription);
+            }
+            return sortInfos;
+        }
+
+        void SetColumnInfo(DataGrid dg, List<DataGridColumn> columnInfos)
+        {
+            columnInfos.Sort((c1, c2) => { return c1.DisplayIndex - c2.DisplayIndex; });
+            foreach (var columnInfo in columnInfos)
+            {
+                var column = dg.Columns.FirstOrDefault(col => col.Header == columnInfo.Header);
+                if (column != null)
+                {
+                    column.SortDirection = columnInfo.SortDirection;
+                    column.DisplayIndex = columnInfo.DisplayIndex;
+                    column.Visibility = columnInfo.Visibility;
+                }
+            }
+        }
+
+        void SetSortInfo(DataGrid dg, List<SortDescription> sortInfos)
+        {
+            dg.Items.SortDescriptions.Clear();
+            foreach (var sortInfo in sortInfos)
+            {
+                dg.Items.SortDescriptions.Add(sortInfo);
+            }
+        }
+
+    }
+}
---
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/WMIBrowser.xaml.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+using sccmclictr.automation;
+using sccmclictr.automation.functions;
+using System.Management.Automation;
+
+namespace ClientCenter.Controls
+{
+    /// <summary>
+    /// Interaction logic for WMIBrowser.xaml
+    /// </summary>
+    public partial class WMIBrowser : UserControl
+    {
+        private SCCMAgent oAgent;
+        public MyTraceListener Listener;
+        List<string> lClasses = new List<string>();
+        public List<string> lAdhocQueries = new List<string>();
+
+        public SCCMAgent SCCMAgentConnection
+        {
+            get
+            {
+                return oAgent;
+            }
+            set
+            {
+                if (value.isConnected)
+                {
+                    Mouse.OverrideCursor = Cursors.Wait;
+                    try
+                    {
+                        oAgent = value;
+                        miAdhocInv.Items.Clear();
+                        foreach(string sInv in lAdhocQueries)
+                        {
+                            MenuItem mi = new MenuItem();
+                            mi.Header = sInv.Split('|')[0];
+                            mi.Tag = sInv;
+                            miAdhocInv.Items.Add(mi);
+                        }
+                    }
+                    catch { }
+                    Mouse.OverrideCursor = Cursors.Arrow;
+                }
+            }
+        }
+
+        public WMIBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void cb_Namespace_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                if (oAgent.isConnected)
+                {
+                    lClasses.Clear();
+
+                    List<PSObject> oClasses = oAgent.Client.GetObjectsFromPS("Get-WmiObject -Namespace '" + ((ListBoxItem)cb_Namespace.SelectedValue).Content.ToString() + "' -List | where { !$_.Name.StartsWith(\"__\") }", false);
+
+                    foreach (PSObject oClass in oClasses)
+                    {
+                        lClasses.Add(oClass.Properties["Name"].Value.ToString());
+                    }
+
+                    cb_Classes.ItemsSource = lClasses.OrderBy(t => t);
+                }
+            }
+            catch { }
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void cb_Classes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                if (oAgent.isConnected)
+                {
+                    TreeViewItem tvMain = null;
+
+                    foreach (TreeViewItem tvi in tvObjects.Items)
+                    {
+                        if (tvi.Header == cb_Classes.SelectedValue.ToString())
+                        {
+                            tvMain = tvi;
+                            continue;
+                        }
+                    }
+
+                    if (tvMain == null)
+                    {
+                        tvMain = new TreeViewItem();
+                        tvMain.Header = cb_Classes.SelectedValue.ToString();
+                        //tvMain.Name = cb_Classes.SelectedValue.ToString();
+                        tvObjects.Items.Add(tvMain);
+                    }
+                    else
+                    {
+                        tvMain.Items.Clear();
+                    }
+
+                    List<PSObject> oObj = oAgent.Client.GetObjects(cb_Namespace.Text, "SELECT * FROM " + cb_Classes.SelectedValue.ToString(), false);
+
+
+                    foreach (PSObject PSObj in oObj)
+                    {
+                        try
+                        {
+                            System.Collections.ObjectModel.ObservableCollection<wmiProp> oColl = new System.Collections.ObjectModel.ObservableCollection<wmiProp>();
+                            PSObj.Properties.Where(w => w.GetType() == typeof(PSProperty) & !w.Name.StartsWith("_")).ToList().ForEach(x => oColl.Add(new wmiProp(x.Name, x.Value)));
+
+                            //Dictionary<string, string> obj = PSObj.Properties.Where(w => w.GetType() == typeof(PSProperty) & !w.Name.StartsWith("_")).ToList
+
+                            /*Dictionary<string, string> obj = PSObj.Properties.Where(w => w.GetType() == typeof(PSProperty) & !w.Name.StartsWith("_")).ToDictionary(t => t.Name, x => x.Value.ToString());
+                            foreach(KeyValuePair<string, string> sVal in obj)
+                            {
+                                oColl.Add(new wmiProp(sVal.Key, sVal.Value));
+                            }*/
+
+                            DataGrid dg = new DataGrid();
+                            dg.CanUserSortColumns = true;
+                            dg.CanUserResizeColumns = true;
+                            dg.AutoGenerateColumns = true;
+                            dg.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                            dg.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+                            dg.SelectionUnit = DataGridSelectionUnit.CellOrRowHeader;
+
+                            dg.ItemsSource = oColl;
+
+                            TreeViewItem tvi = new TreeViewItem();
+                            tvi.Header = PSObj.Properties["__RELPATH"].Value;
+                            tvi.Items.Add(dg);
+
+                            tvMain.Items.Add(tvi);
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex.Message);
+                        }
+                    }
+
+                }
+            }
+            catch { }
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        public class wmiProp
+        {
+            private string _Value;
+            public wmiProp(string sKey, string sValue)
+            {
+                Property = sKey;
+                if (!string.IsNullOrEmpty(sValue))
+                    Value = sValue;
+                else
+                    Value = "";
+            }
+
+            public wmiProp(string sKey, object oValue)
+            {
+                Property = sKey;
+                if (oValue != null)
+                    Value = oValue.ToString();
+                else
+                    Value = "";
+            }
+            public string Property { get; set; }
+            public string Value 
+            { 
+                get
+                {
+                    return _Value;
+                } 
+                set
+                {
+                    _Value = value;
+
+                    //Check if it's in a DateTime Format
+                    if (value.Length == 25)
+                    {
+                        if (value[14] == '.')
+                        {
+                            _Value = System.Management.ManagementDateTimeConverter.ToDateTime(value).ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void bt_ClearWMIBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                tvObjects.Items.Clear();
+                //cb_Namespace.SelectedIndex = -1;
+                cb_Classes.SelectedIndex = -1;
+            }
+            catch { }
+
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        private void miAdhocInv_Click(object sender, RoutedEventArgs e)
+        {
+            string sTag = ((MenuItem)e.Source).Tag.ToString();
+            string sName = sTag.Split('|')[0];
+            string sNamespace = sTag.Split('|')[1];
+            string sQuery = sTag.Split('|')[2];
+
+            Mouse.OverrideCursor = Cursors.Wait;
+            try
+            {
+                if (oAgent.isConnected)
+                {
+                    TreeViewItem tvMain = null;
+
+                    foreach (TreeViewItem tvi in tvObjects.Items)
+                    {
+                        if (tvi.Header == sName)
+                        {
+                            tvMain = tvi;
+                            continue;
+                        }
+                    }
+
+                    if (tvMain == null)
+                    {
+                        tvMain = new TreeViewItem();
+                        tvMain.Header = sName;
+                        tvObjects.Items.Add(tvMain);
+                    }
+                    else
+                    {
+                        tvMain.Items.Clear();
+                    }
+
+                    List<PSObject> oObj = oAgent.Client.GetObjects(sNamespace, sQuery, false);
+
+
+                    foreach (PSObject PSObj in oObj)
+                    {
+                        System.Collections.ObjectModel.ObservableCollection<wmiProp> oColl = new System.Collections.ObjectModel.ObservableCollection<wmiProp>();
+                        PSObj.Properties.Where(w => w.GetType() == typeof(PSProperty) & !w.Name.StartsWith("_")).ToList().ForEach(x => oColl.Add(new wmiProp(x.Name, x.Value)));
+
+                        DataGrid dg = new DataGrid();
+                        dg.CanUserSortColumns = true;
+                        dg.CanUserResizeColumns = true;
+                        dg.SelectionUnit = DataGridSelectionUnit.CellOrRowHeader;
+                        dg.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                        dg.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+                        dg.ItemsSource = oColl;
+
+                        //TreeViewItem tvi = new TreeViewItem();
+                        //tvi.Header = PSObj.Properties["__RELPATH"].Value;
+                        //tvi.Items.Add(dg);
+                        tvMain.Items.Add(dg);
+                        tvMain.ExpandSubtree();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+    }
+}
