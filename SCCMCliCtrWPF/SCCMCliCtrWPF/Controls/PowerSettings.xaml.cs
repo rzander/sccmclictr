c57ac86b8195d736cd5fe0fd637d8fe101e61752
rzander_cp <SND\rzander_cp>
2014-03-28 14:49:20 +0000

    New: PowerSettings Tab implemented

---
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/PowerSettings.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+using sccmclictr.automation;
+using sccmclictr.automation.functions;
+
+namespace ClientCenter
+{
+    /// <summary>
+    /// Interaction logic for ServicesGrid.xaml
+    /// </summary>
+    public partial class PowerSettings : UserControl
+    {
+        private SCCMAgent oAgent;
+        public MyTraceListener Listener;
+
+        public Boolean OnlyCMServices = false;
+
+        public PowerSettings()
+        {
+            InitializeComponent();
+        }
+        internal List<sccmclictr.automation.functions.inventory.SMS_PowerSettings> iPWR;
+
+        public SCCMAgent SCCMAgentConnection
+        {
+            get
+            {
+                return oAgent;
+            }
+            set
+            {
+                if (value.isConnected)
+                {
+                    Mouse.OverrideCursor = Cursors.Wait;
+                    try
+                    {
+                        oAgent = value;
+                        iPWR = oAgent.Client.Inventory.PowerSettings(false).OrderBy(p=>p.Name).ToList();
+
+                        dataGrid1.BeginInit();
+                        dataGrid1.ItemsSource = iPWR;
+                        dataGrid1.EndInit();
+                    }
+                    catch { }
+                    Mouse.OverrideCursor = Cursors.Arrow;
+                }
+            }
+        }
+
+    }
+}
