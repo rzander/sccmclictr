a2bf053b147c7641eeb1756d608ae1a1f020826d
rzander_cp <SND\rzander_cp>
2014-03-31 14:25:22 +0000

    Fix: CacheItems keep sorting

--- SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/CacheGrid.xaml.cs
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/CacheGrid.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 
 using System.Diagnostics;
 using sccmclictr.automation;
+using System.ComponentModel;
 
 namespace ClientCenter
 {
@@ -41,6 +42,7 @@ namespace ClientCenter
                 if (value.isConnected)
                 {
                     Mouse.OverrideCursor = Cursors.Wait;
+                    List<SortDescription> ssort = GetSortInfo(dataGrid1);
                     try
                     {
                         oAgent = value;
@@ -60,6 +62,7 @@ namespace ClientCenter
                         sbiContentSize.Content = ((iTotalSize)/1024).ToString() + " (MB)";
                     }
                     catch { }
+                    SetSortInfo(dataGrid1, ssort);
                     Mouse.OverrideCursor = Cursors.Arrow;
                 }
             }
@@ -235,6 +238,50 @@ namespace ClientCenter
 
         private void dataGrid1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+        }
+
+        List<DataGridColumn> GetColumnInfo(DataGrid dg)
+        {
+            List<DataGridColumn> columnInfos = new List<DataGridColumn>();
+            foreach (var column in dg.Columns)
+            {
+                columnInfos.Add(column);
+            }
+            return columnInfos;
+        }
+
+        List<SortDescription> GetSortInfo(DataGrid dg)
+        {
+            List<SortDescription> sortInfos = new List<SortDescription>();
+            foreach (var sortDescription in dg.Items.SortDescriptions)
+            {
+                sortInfos.Add(sortDescription);
+            }
+            return sortInfos;
+        }
+
+        void SetColumnInfo(DataGrid dg, List<DataGridColumn> columnInfos)
+        {
+            columnInfos.Sort((c1, c2) => { return c1.DisplayIndex - c2.DisplayIndex; });
+            foreach (var columnInfo in columnInfos)
+            {
+                var column = dg.Columns.FirstOrDefault(col => col.Header == columnInfo.Header);
+                if (column != null)
+                {
+                    column.SortDirection = columnInfo.SortDirection;
+                    column.DisplayIndex = columnInfo.DisplayIndex;
+                    column.Visibility = columnInfo.Visibility;
+                }
+            }
+        }
+
+        void SetSortInfo(DataGrid dg, List<SortDescription> sortInfos)
+        {
+            dg.Items.SortDescriptions.Clear();
+            foreach (var sortInfo in sortInfos)
+            {
+                dg.Items.SortDescriptions.Add(sortInfo);
+            }
         }
     }
 }
