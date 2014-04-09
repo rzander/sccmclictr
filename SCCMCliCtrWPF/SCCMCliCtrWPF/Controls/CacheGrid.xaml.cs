5b7832f30c4bf3ec697c01a32d4dfadb724d87cf
rzander_cp <SND\rzander_cp>
2014-04-09 14:24:26 +0000

    keep Cache Grid sorting

--- SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/CacheGrid.xaml.cs
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/CacheGrid.xaml.cs
@@ -59,7 +59,7 @@ namespace ClientCenter
                                 iTotalSize = iTotalSize + CacheItem.ContentSize;
                             }
                         }
-                        sbiContentSize.Content = ((iTotalSize)/1024).ToString() + " (MB)";
+                        sbiContentSize.Content = ((iTotalSize) / 1024).ToString() + " (MB)";
                     }
                     catch { }
                     SetSortInfo(dataGrid1, ssort);
@@ -238,6 +238,50 @@ namespace ClientCenter
 
         private void dataGrid1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+        }
+
+        List<DataGridColumn> GetColumnInfo(DataGrid dg)
+        {
+            List<DataGridColumn> columnInfos = new List<DataGridColumn>();
+            foreach (var column in dg.Columns)
+            {
+                columnInfos.Add(column);
+            }
+            return columnInfos;
+        }
+
+        List<SortDescription> GetSortInfo(DataGrid dg)
+        {
+            List<SortDescription> sortInfos = new List<SortDescription>();
+            foreach (var sortDescription in dg.Items.SortDescriptions)
+            {
+                sortInfos.Add(sortDescription);
+            }
+            return sortInfos;
+        }
+
+        void SetColumnInfo(DataGrid dg, List<DataGridColumn> columnInfos)
+        {
+            columnInfos.Sort((c1, c2) => { return c1.DisplayIndex - c2.DisplayIndex; });
+            foreach (var columnInfo in columnInfos)
+            {
+                var column = dg.Columns.FirstOrDefault(col => col.Header == columnInfo.Header);
+                if (column != null)
+                {
+                    column.SortDirection = columnInfo.SortDirection;
+                    column.DisplayIndex = columnInfo.DisplayIndex;
+                    column.Visibility = columnInfo.Visibility;
+                }
+            }
+        }
+
+        void SetSortInfo(DataGrid dg, List<SortDescription> sortInfos)
+        {
+            dg.Items.SortDescriptions.Clear();
+            foreach (var sortInfo in sortInfos)
+            {
+                dg.Items.SortDescriptions.Add(sortInfo);
+            }
         }
 
         List<DataGridColumn> GetColumnInfo(DataGrid dg)
