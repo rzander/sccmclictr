23a7a07acc09766240618ebecabe6746da309785
rzander_cp <SND\rzander_cp>
2014-04-23 12:12:56 +0000

    Application Evaluation over COM Object added

--- SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/CacheGrid.xaml.cs
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/Controls/CacheGrid.xaml.cs
@@ -283,49 +283,5 @@ namespace ClientCenter
                 dg.Items.SortDescriptions.Add(sortInfo);
             }
         }
-
-        List<DataGridColumn> GetColumnInfo(DataGrid dg)
-        {
-            List<DataGridColumn> columnInfos = new List<DataGridColumn>();
-            foreach (var column in dg.Columns)
-            {
-                columnInfos.Add(column);
-            }
-            return columnInfos;
-        }
-
-        List<SortDescription> GetSortInfo(DataGrid dg)
-        {
-            List<SortDescription> sortInfos = new List<SortDescription>();
-            foreach (var sortDescription in dg.Items.SortDescriptions)
-            {
-                sortInfos.Add(sortDescription);
-            }
-            return sortInfos;
-        }
-
-        void SetColumnInfo(DataGrid dg, List<DataGridColumn> columnInfos)
-        {
-            columnInfos.Sort((c1, c2) => { return c1.DisplayIndex - c2.DisplayIndex; });
-            foreach (var columnInfo in columnInfos)
-            {
-                var column = dg.Columns.FirstOrDefault(col => col.Header == columnInfo.Header);
-                if (column != null)
-                {
-                    column.SortDirection = columnInfo.SortDirection;
-                    column.DisplayIndex = columnInfo.DisplayIndex;
-                    column.Visibility = columnInfo.Visibility;
-                }
-            }
-        }
-
-        void SetSortInfo(DataGrid dg, List<SortDescription> sortInfos)
-        {
-            dg.Items.SortDescriptions.Clear();
-            foreach (var sortInfo in sortInfos)
-            {
-                dg.Items.SortDescriptions.Add(sortInfo);
-            }
-        }
     }
 }
