921ae8f337bd4e4c175fcb0aca87638d788b700c
Markus M <weissbierwaldi@gmail.com>
2019-01-09 11:33:09 +0100

    Added BooleanToImageConverter

---
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/Helper/Converter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ClientCenter.Helper
+{
+    public class BooleanToImageConverter : IValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter">Has to contain uris to two images seperated by | </param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Boolean bval = false;
+            Boolean.TryParse(value.ToString(), out bval);
+
+            String[] Images = ((String)parameter).Split('|');
+
+            if(bval)
+                return new BitmapImage(new Uri(Images[0]));
+            else
+                return new BitmapImage(new Uri(Images[1]));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
