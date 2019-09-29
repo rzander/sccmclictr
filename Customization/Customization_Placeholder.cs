fd1d25297e1f1505d739ef7a992f30c63b3fba46
blue <playbynumbers@gmail.com>
2019-09-29 12:07:19 +0100

    Move Customization to separate project
    Allows plugins to depend directly on customization rather than the
    whole of SCCMCliCtrWPF, meaning it can then reference plugins to
    automatically include them in builds.
    
    The new project is placed in the root of the repo as there are
    existing references to a Customization project there, eg several
    plugins already look there for the DLL, so no changes are required
    for them to function.

---
+++ Customization/Customization_Placeholder.cs
@@ -0,0 +1,11 @@
+﻿namespace SCCMCliCtr
+{
+    public class Customization
+    {
+        public static string Title = "Client Center for ConfigMgr (OpenSource Version)";
+
+        public static bool CheckLicense() { return false; }
+
+        public static bool isOpenSource = true;
+    }
+}
