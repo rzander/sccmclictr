550a8fb01bc889e83074575eed127bcd346b908d
Roger Zander <roger@zander.ch>
2019-10-16 19:34:52 +0200

    Update references

--- SCCMCliCtrWPF/SCCMCliCtrWPF/Properties/AssemblyInfo.cs
+++ SCCMCliCtrWPF/SCCMCliCtrWPF/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.5.*")]
-[assembly: AssemblyFileVersion("1.0.5.4")]
+[assembly: AssemblyFileVersion("1.0.5.5")]
